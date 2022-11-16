25063e5fd7854587d25ee125ea62f1ac63d47bef
Connor Monk <como5366@colorado.edu>
2022-11-16 11:45:46 -0800

    added the picture and the .py and it works

---
+++ cgi-bin/weather_script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+print("Content-type: text/html")
+print() # This extra newline is important!
+
+print("<html>")
+
+print("<head><title>Weather Map Lab by Your last name</title></head>")
+
+print('<body>')
+print('<img src=\"../Blank_US_Map_(states_only).svg\" alt=\"Blank_US_Map_\">')
+print('</body>')
+print('</html>')
